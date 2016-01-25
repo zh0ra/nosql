21dfaa02ec8447b272b55c17146296294507756b
Oleksandr <oleksandr.zhorniak@gmail.com>
2016-01-25 13:58:36 +0100

    Correct in postgres scripts...

--- scripts/copy_incedent_esponce_911.sql
+++ scripts/copy_incedent_esponce_911.sql
@@ -1,26 +1,26 @@
-/*
+﻿/*
 Import dataset from Seattle_Police_Department_911_Incident_Response.csv
 */
-COPY incedent_esponce_911 (
- "CAD CDW ID",
- "CAD Event Number",
- "General Offense Number",
- "Event Clearance Code",
- "Event Clearance Description",
- "Event Clearance SubGroup",
- "Event Clearance Group",
- "Event Clearance Date",
- "Hundred Block Location",
- "District/Sector",
- "Zone/Beat",
- "Census Tract",
- "Longitude",
- "Latitude",
- "Incident Location",
- "Initial Type Description",
- "Initial Type Subgroup",
- "Initial Type Group",
- "At Scene Time"
+COPY incedent_responce_911 (
+ CAD_CDW_ID,
+ CAD_Event_Number,
+ General_Offense_Number,
+ Event_Clearance_Code,
+ Event_Clearance_Description,
+ Event_Clearance_SubGroup,
+ Event_Clearance_Group,
+ Event_Clearance_Date,
+ Hundred_Block_Location,
+ District_Sector,
+ Zone_Beat,
+ Census_Tract,
+ Longitude,
+ Latitude,
+ Incident_Location,
+ Initial_Type_Description,
+ Initial_Type_Subgroup,
+ Initial_Type_Group,
+ At_Scene_Time
 ) FROM '/home/zhora/data/Seattle/Seattle_Police_Department_911_Incident_Response.csv'
 WITH DELIMITER ','
 CSV HEADER;
--- scripts/create_table_incedent_esponce_911.sql
+++ scripts/create_table_incedent_esponce_911.sql
@@ -1,4 +1,4 @@
-/*
+﻿/*
 Preparing Postgresql for inmort dataset from file: Seattle_Police_Department_911_Incident_Response.csv
 Create data base
 */
@@ -7,25 +7,24 @@ Create data base
 CREATE DATABASE seattlepd;
 
 #Second step, create table for importing dataset
-CREATE TABLE incedent_esponce_911 (
-"_id" NUMERIC(25) PRIMARY KEY,
- "CAD CDW ID" VARCHAR(25),
- "CAD Event Number" VARCHAR(25),
- "General Offense Number" VARCHAR(25),
- "Event Clearance Code" VARCHAR(25),
- "Event Clearance Description" VARCHAR(250),
- "Event Clearance SubGroup" VARCHAR(50),
- "Event Clearance Group" VARCHAR(50),
- "Event Clearance Date" VARCHAR(50),
- "Hundred Block Location" VARCHAR(250),
- "District/Sector" VARCHAR(4),
- "Zone/Beat" VARCHAR(10),
- "Census Tract" VARCHAR(15),
- "Longitude" FLOAT(5),
- "Latitude" FLOAT(5),
- "Incident Location" VARCHAR(50),
- "Initial Type Description" VARCHAR(250),
- "Initial Type Subgroup" VARCHAR(150),
- "Initial Type Group" VARCHAR(150),
- "At Scene Time" VARCHAR(25)
+CREATE TABLE incedent_responce_911 (
+ CAD_CDW_ID VARCHAR(25),
+ CAD_Event_Number VARCHAR(25),
+ General_Offense_Number VARCHAR(25),
+ Event_Clearance_Code VARCHAR(25),
+ Event_Clearance_Description VARCHAR(250),
+ Event_Clearance_SubGroup VARCHAR(50),
+ Event_Clearance_Group VARCHAR(50),
+ Event_Clearance_Date VARCHAR(50),
+ Hundred_Block_Location VARCHAR(250),
+ District_Sector VARCHAR(4),
+ Zone_Beat VARCHAR(10),
+ Census_Tract VARCHAR(15),
+ Longitude FLOAT(5),
+ Latitude FLOAT(5),
+ Incident_Location VARCHAR(50),
+ Initial_Type_Description VARCHAR(250),
+ Initial_Type_Subgroup VARCHAR(150),
+ Initial_Type_Group VARCHAR(150),
+ At_Scene_Time VARCHAR(25)
 );
